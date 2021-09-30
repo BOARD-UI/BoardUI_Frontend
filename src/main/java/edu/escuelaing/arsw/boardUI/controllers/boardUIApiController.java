adb553fec635c93407f9adec3d83fa2b8f11c93c
J-382 <jrperezdeleon@gmail.com>
2021-09-29 23:02:00 -0500

    Esqueleto del proyecto, index.html

---
+++ src/main/java/edu/escuelaing/arsw/boardUI/controllers/boardUIApiController.java
@@ -0,0 +1,12 @@
+package edu.escuelaing.arsw.boardUI.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping(value = "/")
+public class boardUIApiController {
+    
+}
---
+++ src/main/java/edu/escuelaing/arsw/boardUIApi/BoardUIApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.boardUIApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"edu.eci.arsw.boardUI"})
+public class BoardUIApplication {
+    public static void main(String[] args) {
+		SpringApplication.run(BoardUIApplication.class, args);
+	}
+}
