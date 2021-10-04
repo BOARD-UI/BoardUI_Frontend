6bd419e78fbcb4c7de3c16836fc2ba04a5a91740
Luis Amaya <luis.amaya-o@mail.escuelaing.edu.co>
2021-10-03 22:04:21 -0500

    Fix number 2

---
+++ src/main/java/edu/escuelaing/arsw/boardUI/model/BoardUIApiController.java
@@ -0,0 +1,25 @@
+package edu.escuelaing.arsw.boardUI.model;
+
+import javax.json.JsonObject;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+public class BoardUIApiController {
+
+    @RequestMapping(path = "/welcome/NewUser", method = RequestMethod.POST)
+    public ResponseEntity<?> CreateNewUser(@RequestBody JsonObject newUser) {
+        try {
+            System.out.println(newUser);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+
+    }
+}
