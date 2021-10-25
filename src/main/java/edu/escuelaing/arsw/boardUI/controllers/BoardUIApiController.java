0f4e95031da2de307ae551469c584e661cb8fffb
J-382 <jrperezdeleon@gmail.com>
2021-10-25 03:53:49 -0500

    Test connetions 2

--- src/main/java/edu/escuelaing/arsw/boardUI/controllers/BoardUIApiController.java
+++ src/main/java/edu/escuelaing/arsw/boardUI/controllers/BoardUIApiController.java
@@ -1,19 +1,24 @@
 package edu.escuelaing.arsw.boardUI.controllers;
 
-import javax.json.JsonObject;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.escuelaing.arsw.boardUI.model.User;
+import edu.escuelaing.arsw.boardUI.services.BoardUIServicesException;
+import edu.escuelaing.arsw.boardUI.services.impl.UserServices;
 
 import org.springframework.http.ResponseEntity;
 
 @RestController
 public class BoardUIApiController {
+
+    @Autowired
+    UserServices us;
 
     @RequestMapping(path = "/welcome/NewUser", method = RequestMethod.POST)
     public ResponseEntity<?> CreateNewUser(@RequestBody User newUser) {
@@ -23,6 +28,12 @@ public class BoardUIApiController {
         } catch (Exception ex) {
             return new ResponseEntity<>(HttpStatus.CREATED);
         }
+    }
+
+    @RequestMapping(path = "/public/user/{username}", method = RequestMethod.GET)
+    public ResponseEntity<?> getHomePage(@PathVariable("username") String username) throws BoardUIServicesException {
+        User user = us.getUserByUsername(username);
+        return new ResponseEntity<>(user.toString(),HttpStatus.CREATED);
     }
 
 	
